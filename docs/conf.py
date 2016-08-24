f35008ccce1220fabd5d8de24a507415f3d801f7
Flávio Codeço Coelho <fccoelho@gmail.com>
2016-08-24 10:11:51 -0300

    tweaked conf.py to try to fix readthedocs build

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Hack for ReadTheDocs ------------------------------------------------------
 # This hack is necessary since RTD does not issue `sphinx-apidoc` before running
