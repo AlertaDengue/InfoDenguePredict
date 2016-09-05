0b894cf38177ade3c799fcfd3c71ea9c6a89f498
Flávio Codeço Coelho <fccoelho@gmail.com>
2016-09-05 15:32:38 -0300

    Added functions to fetch tweet, and weather underground data

--- infodenguepredict/data/infodengue.py
+++ infodenguepredict/data/infodengue.py
@@ -1,6 +1,6 @@
 """
 This module performs data fetching from the Infodengue database.
-for remote database access, we recomment establishing an SSH tunnel:
+for remote database access, we recommend establishing an SSH tunnel:
 ssh -f user@remote-server -L 5432:localhost:5432 -N
 """
 
@@ -29,7 +29,28 @@ def get_alerta_table(municipio=None):
     return df
 
 def get_temperature_data(municipio=None):
-    pass
+    conexao = create_engine("postgresql://{}:{}@{}/{}".format(config('PSQL_USER'),
+                                                              config('PSQL_PASSWORD'),
+                                                              config('PSQL_HOST'),
+                                                              config('PSQL_DB')))
+
+    if municipio is None:
+        df = pd.read_sql_query('select * from "Municipio"."Clima_wu" ORDER BY "data_dia" ASC;',
+                                conexao, index_col='id')
+    else:
+        df = pd.read_sql_query('select cwu.id, temp_min, umid_min, pressao_min, data_dia FROM "Municipio"."Clima_wu" cwu JOIN "Dengue_global".regional_saude rs ON cwu."Estacao_wu_estacao_id"=rs.codigo_estacao_wu WHERE rs.municipio_geocodigo={} ORDER BY data_dia ASC;'.format(municipio), conexao, index_col='id')
+    df.set_index('data_dia', inplace=True)
+    return df
 
 def get_tweet_data(municipio=None):
-    pass
+    conexao = create_engine("postgresql://{}:{}@{}/{}".format(config('PSQL_USER'),
+                                                              config('PSQL_PASSWORD'),
+                                                              config('PSQL_HOST'),
+                                                              config('PSQL_DB')))
+    if municipio is None:
+        df = pd.read_sql_query('select * from "Municipio"."Tweet" ORDER BY "data_dia" ASC;',
+                                conexao, index_col='id')
+    else:
+        df = pd.read_sql_query('select * FROM "Municipio"."Tweet" WHERE "Municipio_geocodigo"={} ORDER BY data_dia ASC;'.format(municipio), conexao, index_col='id')
+    df.set_index('data_dia', inplace=True)
+    return df
---
+++ tests/test_data.py
@@ -0,0 +1,29 @@
+"""
+Tests related to data fetching code
+"""
+
+__author__ = 'fccoelho@gmail.com'
+
+import unittest
+from infodenguepredict.data.infodengue import get_temperature_data, get_alerta_table, get_tweet_data
+import pandas as pd
+
+class TestInfodengue(unittest.TestCase):
+    def test_temp_data(self):
+        df = get_temperature_data(3303609)
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertGreater(df.size, 0)
+
+    def test_tweet(self):
+        df = get_tweet_data(3303609)
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertGreater(df.size, 0)
+
+    def test_alerta(self):
+        df = get_alerta_table(3303609)
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertGreater(df.size, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
