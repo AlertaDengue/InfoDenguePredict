a6954bffa041e414312d7121ee25a5b0a98b3974
Flavio C. Coelho <fccoelho@gmail.com>
2016-12-08 10:51:30 -0200

    added module to handle downloads of sattelite imagery.

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,7 +4,10 @@ Tests related to data fetching code
 
 import unittest
 from infodenguepredict.data.infodengue import get_temperature_data, get_alerta_table, get_tweet_data
+from infodenguepredict.data.satellite import  LandSurfaceTemperature
 import pandas as pd
+import os
+from glob import glob
 
 
 __author__ = 'fccoelho@gmail.com'
@@ -25,6 +28,15 @@ class TestInfodengue(unittest.TestCase):
         df = get_alerta_table(3303609)
         self.assertIsInstance(df, pd.DataFrame)
         self.assertGreater(df.size, 0)
+
+class TestSatellite(unittest.TestCase):
+    def test_get_5d_image(self):
+        fetcher = LandSurfaceTemperature()
+        fetcher.get_5day_average_image(44.01104, 42.5841, 23.0134, 22.4916, start_date='20160101', end_date='20160106')
+        files = glob('*.tiff')
+        self.assertGreater(len(files), 0)
+        os.system("rm *.tiff*")
+
 
 
 if __name__ == '__main__':
