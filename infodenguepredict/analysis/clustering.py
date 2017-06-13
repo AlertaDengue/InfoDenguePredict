aefe8c66bd22246a38bbbb401360acbc7fa1921a
Elisa <elisamussumeci@gmail.com>
2017-06-13 15:22:05 -0300

    Fix clustering graph labels

--- infodenguepredict/analysis/clustering.py
+++ infodenguepredict/analysis/clustering.py
@@ -1,15 +1,16 @@
 import numpy as np;
 import seaborn as sns;
 import pandas as pd
+import re
 from scipy import stats
 import scipy.cluster.hierarchy as hac
 import matplotlib.pyplot as plt
 from infodenguepredict.data.infodengue import get_alerta_table, build_multicity_dataset
 
 
+
 def hierarchical_clustering(df, method='complete'):
     """
-
     :param method: Clustering method
     :param df: Dataframe with series as columns
     :return:
@@ -23,6 +24,13 @@ if __name__ == "__main__":
     data = data[[col for col in data.columns if col.startswith('casos') and not col.startswith('casos_est')]]
     Z = hierarchical_clustering(data)
     print(len(data.columns), Z.shape)
+
+    dic = pd.read_excel('../data/codigos_rj.xlsx', names=['city','code'], header=None).set_index(
+        'code')
+    dic.index = dic.index.astype('str')
+    codes_dict = dic.to_dict()['city']
+    labels = [codes_dict[re.sub('casos_','',i)] for i in data.columns]
+
     plt.figure(figsize=(25, 10))
     plt.title('Hierarchical Clustering Dendrogram')
     plt.xlabel('sample index')
@@ -31,6 +39,6 @@ if __name__ == "__main__":
         Z,
         leaf_rotation=90.,  # rotates the x axis labels
         leaf_font_size=8.,  # font size for the x axis labels
-        labels= data.columns,
+        labels=labels,
     )
     plt.show()
