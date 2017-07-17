383e28dd16c3aa2ec715f7766a73e9478b529b98
elisa <elisamussumeci@gmail.com>
2017-07-17 16:31:53 -0300

    Change input type of clustering from point to correlation distance matrix

--- infodenguepredict/analysis/clustering.py
+++ infodenguepredict/analysis/clustering.py
@@ -4,10 +4,10 @@ import pandas as pd
 import re
 from scipy import stats
 import scipy.cluster.hierarchy as hac
+from scipy.spatial import distance as ssd
 import matplotlib.pyplot as plt
 from infodenguepredict.data.infodengue import get_alerta_table, build_multicity_dataset
-
-
+from infodenguepredict.analysis.distance import distance, alocate_data
 
 def hierarchical_clustering(df, method='complete'):
     """
@@ -15,21 +15,21 @@ def hierarchical_clustering(df, method='complete'):
     :param df: Dataframe with series as columns
     :return:
     """
-    Z = hac.linkage(df.values.T, method=method, metric='correlation')
+    Z = hac.linkage(ssd.squareform(df.values.T + df.values), method=method)
     return Z
 
 
 if __name__ == "__main__":
-    data = build_multicity_dataset('RJ')
-    data = data[[col for col in data.columns if col.startswith('casos') and not col.startswith('casos_est')]]
-    Z = hierarchical_clustering(data)
-    print(len(data.columns), Z.shape)
+    cities_list = alocate_data("RJ")
+    dists = distance(cities_list)
+    Z = hierarchical_clustering(dists)
+    # print(len(data.columns), Z.shape)
 
     dic = pd.read_excel('../data/codigos_rj.xlsx', names=['city','code'], header=None).set_index(
         'code')
     dic.index = dic.index.astype('str')
     codes_dict = dic.to_dict()['city']
-    labels = [codes_dict[re.sub('casos_','',i)] for i in data.columns]
+    labels = [codes_dict[str(i)] for i in dists.columns]
 
     plt.figure(figsize=(25, 10))
     plt.title('Hierarchical Clustering Dendrogram')
