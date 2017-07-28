c665a4a5aa8b0fcb7f34e9e4e1510dab7fb6be99
elisa <elisamussumeci@gmail.com>
2017-07-28 12:39:26 -0300

    Change input of clustering from dataframe to distance matrix

--- infodenguepredict/analysis/clustering.py
+++ infodenguepredict/analysis/clustering.py
@@ -1,35 +1,34 @@
-import numpy as np;
-import seaborn as sns;
 import pandas as pd
-import re
-from scipy import stats
 import scipy.cluster.hierarchy as hac
 from scipy.spatial import distance as ssd
 import matplotlib.pyplot as plt
-from infodenguepredict.data.infodengue import get_alerta_table, build_multicity_dataset
 from infodenguepredict.analysis.distance import distance, alocate_data
 
 def hierarchical_clustering(df, method='complete'):
     """
     :param method: Clustering method
-    :param df: Dataframe with series as columns
+    :param df: Triangular distances matrix
     :return:
     """
     Z = hac.linkage(ssd.squareform(df.values.T + df.values), method=method)
-    return Z
+
+    ind = hac.fcluster(Z, 0.7 * max(Z[:, 2]), 'distance')
+    grouped = pd.DataFrame(list(zip(ind, df.index))).groupby(0)
+    clusters = [group[1][1].values for group in grouped]
+    return Z, clusters
 
 
 if __name__ == "__main__":
     cities_list = alocate_data("RJ")
     dists = distance(cities_list)
-    Z = hierarchical_clustering(dists)
+    Z, clusters = hierarchical_clustering(dists)
     # print(len(data.columns), Z.shape)
 
-    dic = pd.read_excel('../data/codigos_rj.xlsx', names=['city','code'], header=None).set_index(
-        'code')
-    dic.index = dic.index.astype('str')
-    codes_dict = dic.to_dict()['city']
-    labels = [codes_dict[str(i)] for i in dists.columns]
+    # dic = pd.read_excel('../data/codigos_rj.xlsx', names=['city','code'], header=None).set_index(
+    #     'code')
+    # dic.index = dic.index.astype('str')
+    # codes_dict = dic.to_dict()['city']
+    # labels = [codes_dict[str(i)] for i in dists.columns]
 
     plt.figure(figsize=(25, 10))
     plt.title('Hierarchical Clustering Dendrogram')
@@ -39,6 +38,6 @@ if __name__ == "__main__":
         Z,
         leaf_rotation=90.,  # rotates the x axis labels
         leaf_font_size=8.,  # font size for the x axis labels
-        labels=labels,
+        # labels=labels,
     )
     plt.show()
