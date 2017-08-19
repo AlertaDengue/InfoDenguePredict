b7da34b2daf9c35cb78d1b5a4536b12a2f212129
Elisa <elisamussumeci@gmail.com>
2017-08-19 16:43:29 -0300

    Add script that calculates correlations between epidemy rank and model accuracy

--- infodenguepredict/models/deeplearning/lstm_ranking_correlation.py
+++ infodenguepredict/models/deeplearning/lstm_ranking_correlation.py
@@ -1,23 +1,46 @@
-from infodengue import build_multicity_dataset
+from infodenguepredict.data.infodengue import build_multicity_dataset, get_cluster_data
+from infodenguepredict.models.deeplearning.lstm import single_prediction
 import numpy as np
 import pandas as pd
 import re
-
+import pickle
 
 def rank_cities(state):
     mult = build_multicity_dataset(state)
     cols = list(filter(re.compile('casos_\d+').search, mult.columns))
     mult = mult[cols]
 
-    codes = pd.read_excel('../data/codigos_rj.xlsx'.format(lower(state)),
+    print(mult.head())
+    codes = pd.read_excel('../../data/codigos_{}.xlsx'.format(state),
                           names=['city', 'code'], header=None).set_index('code').T
 
-    ints = pd.DataFrame(index=codes.values[0], columns=['integral'])
+    ints = pd.DataFrame()
     for col in mult.columns:
-        ints.loc[codes[int(re.sub('casos_', '', col))]] = [np.trapz(mult[col])]
+        # ints.loc[codes[int(re.sub('casos_', '', col))]] = [np.trapz(mult[col])]
+        ints[col] = [np.trapz(mult[col])]
+    return ints
 
-    return ints.sort_values('integral', ascending=False)
 
+if __name__ == "__main__":
+    TIME_WINDOW = 4
+    HIDDEN = 64
+    LOOK_BACK = 4
+    BATCH_SIZE = 1
+    prediction_window = 3  # weeks
+    # city = 3303500
+    state = 'RJ'
+    epochs = 10
 
-if __name__=="__main_":
-    ranK_df = rank_cities('RJ')
+    rank = rank_cities(state)
+
+    mapes = []
+
+    for col in rank:
+        city = re.sub('casos_', '', col)
+        metric = single_prediction(int(city), state, predict_n=prediction_window, time_window=TIME_WINDOW, hidden=HIDDEN,
+                                   epochs=epochs)
+        mapes.append(metric)
+
+    rank = rank.T
+    rank['mape'] = mapes
+    rank.to_pickle('rank.pkl')
