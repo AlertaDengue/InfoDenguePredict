3d7f4bfdd6ac58214391dc13a6924fbc66d02c07
Elisa <elisamussumeci@gmail.com>
2017-08-25 14:13:47 -0300

    Change color treshold

--- infodenguepredict/analysis/clustering.py
+++ infodenguepredict/analysis/clustering.py
@@ -1,11 +1,14 @@
 import pickle
 import pandas as pd
+import numpy as np
 import scipy.cluster.hierarchy as hac
 from scipy.spatial import distance as ssd
 import matplotlib.pyplot as plt
 from infodenguepredict.analysis.distance import distance, alocate_data
+from infodenguepredict.data.infodengue import get_city_names
+from functools import lru_cache
 
-def hierarchical_clustering(df, method='complete'):
+def hierarchical_clustering(df, t, method='average'):
     """
     :param method: Clustering method
     :param df: Triangular distances matrix
@@ -13,25 +16,32 @@ def hierarchical_clustering(df, method='complete'):
     """
     Z = hac.linkage(ssd.squareform(df.values.T + df.values), method=method)
 
-    ind = hac.fcluster(Z, 0.7 * max(Z[:, 2]), 'distance')
+    ind = hac.fcluster(Z, t * max(Z[:, 2]), 'distance')
     grouped = pd.DataFrame(list(zip(ind, df.index))).groupby(0)
     clusters = [group[1][1].values for group in grouped]
     return Z, clusters
 
-
-def create_cluster(state):
+@lru_cache(maxsize=None)
+def create_cluster(state, t):
     cities_list = alocate_data(state)
     dists = distance(cities_list)
-    Z, clusters = hierarchical_clustering(dists)
+    Z, clusters = hierarchical_clustering(dists, t=t)
 
     with open('clusters_{}.pkl'.format(state), 'wb') as fp:
         pickle.dump(clusters, fp)
 
     print("{} clusters saved".format(state))
-    return Z, [int(c) for c in cities_list]
+    name_ind = get_city_names(list(dists.index))
+    return Z, name_ind
+
+def llf(id):
+    return name_ind[id][1]
 
 if __name__ == "__main__":
-    Z, geocs = create_cluster("RJ")
+    # for t in np.arange(0.6):
+    #     print(t)
+    t = 0.6
+    Z, name_ind = create_cluster("RJ", t)
 
     plt.figure(figsize=(25, 10))
     plt.title('Hierarchical Clustering Dendrogram')
@@ -41,6 +51,8 @@ if __name__ == "__main__":
         Z,
         leaf_rotation=90.,  # rotates the x axis labels
         leaf_font_size=8.,  # font size for the x axis labels
-        # labels=labels,
+        leaf_label_func=llf,
+        color_threshold=t * max(Z[:, 2])
     )
+    plt.savefig('cluster_{}.png'.format(t))
     plt.show()
