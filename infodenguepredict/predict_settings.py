4bb09b26c56064cf0bfda18faf65e7d4185841fc
Elisa <elisamussumeci@gmail.com>
2017-08-28 14:09:56 -0300

    Add LSTM parameters to settings file

--- infodenguepredict/predict_settings.py
+++ infodenguepredict/predict_settings.py
@@ -14,16 +14,25 @@ cluster_vars = [
     "casos"
 ]
 color_treshold = 0.6 # threshold for coloring the dendrogram
-
-tmp_path = '/tmp'
+tmp_path = '/tmp' #path to temporary files for clustering aux data
 
 #=======LSTM parameters==============
 
 predictors = [
-    "casos",
-    "numero",
-    "temp_min",
-    "umid_min",
-
+    'casos',
+    'p_rt1',
+    'p_inc100k',
+    'numero',
+    'temp_min',
+    'temp_max',
+    'umid_min',
+    'pressao_min'
 ]
+
+TIME_WINDOW = 4
+HIDDEN = 4
+LOOK_BACK = 4
+BATCH_SIZE = 1
+prediction_window = 3  # weeks
 city = 3304557
+epochs = 50
